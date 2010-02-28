50f9c905bd3937e41996d6bc351c01cb86a399e0
Chris Kite <chris@chriskite.com>
2010-02-28 17:57:48 -0600

    filter in toolbar

--- lib/rediscover/frame/browser.rb
+++ lib/rediscover/frame/browser.rb
@@ -10,8 +10,10 @@ module Rediscover
 
       def initialize(app)
         @app = app
+        @key_pattern = '*'
         super(nil, -1, 'Rediscover', DEFAULT_POSITION, Size.new(WINDOW_WIDTH, WINDOW_HEIGHT))
         setup_menu_bar
+        setup_tool_bar
         setup_status_bar
         setup_key_browser
         show
@@ -24,6 +26,18 @@ module Rediscover
         @keys_menu = create_keys_menu
 
         @menu_bar.append(@keys_menu, 'Keys')
+      end
+
+      def setup_tool_bar
+        @tool_bar = create_tool_bar
+        @filter_textbox = TextCtrl.new(@tool_bar,
+                                       :style => TE_PROCESS_ENTER,
+                                       :size => Size.new(200, DEFAULT_SIZE.get_height))
+        evt_text_enter @filter_textbox, :filter # filter when enter is pressed in the textbox
+        @filter_button = Button.new(@tool_bar, :label => 'Filter')
+        evt_button @filter_button, :filter # filter when button is pressed
+        @tool_bar.add_control(@filter_textbox)
+        @tool_bar.add_control(@filter_button)
       end
 
       def setup_status_bar
@@ -43,7 +57,7 @@ module Rediscover
       end
 
       def update_key_browser
-        @key_list.set_keys(@app.redis.keys('*'))
+        @key_list.set_keys(@app.redis.keys(@key_pattern))
       end
 
       def create_keys_menu
@@ -66,6 +80,12 @@ module Rediscover
       def refresh
         update_key_browser
         update_status_bar
+      end
+
+      def filter
+        @key_pattern = @filter_textbox.get_value.strip
+        @key_pattern = '*' if @key_pattern.nil? or @key_pattern == ''
+        refresh
       end
 
     end
