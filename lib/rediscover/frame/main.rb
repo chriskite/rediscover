a5ab4527132bc6e2773a800f3802809a9065ee8f
Chris Kite <chris@chriskite.com>
2010-03-03 13:06:50 -0600

    server info tab

--- lib/rediscover/frame/main.rb
+++ lib/rediscover/frame/main.rb
@@ -1,3 +1,4 @@
+require 'rediscover/panel/server'
 require 'rediscover/panel/browser'
 require 'rediscover/frame/create_key'
 
@@ -16,29 +17,18 @@ module Rediscover
         @logger = get_app.logger
 
         setup_notebook
-        setup_tool_bar
         setup_status_bar
         show
       end
 
       def setup_notebook
         @notebook = Notebook.new(self)
+        
+        @server_page = Panel::Server.new(@notebook)
         @browser_page = Panel::Browser.new(@notebook)
 
-        @notebook.add_page(@browser_page, 'Key Browser')
-      end
-
-      def setup_tool_bar
-        @tool_bar = create_tool_bar
-        @filter_textbox = TextCtrl.new(@tool_bar,
-                                       :style => TE_PROCESS_ENTER,
-                                       :size => Size.new(200, DEFAULT_SIZE.get_height))
-        evt_text_enter @filter_textbox, :filter # filter when enter is pressed in the textbox
-        @filter_button = Button.new(@tool_bar, :label => 'Filter')
-        evt_button @filter_button, :filter # filter when button is pressed
-        @tool_bar.add_control(@filter_textbox)
-        @tool_bar.add_control(@filter_button)
-        @tool_bar.realize
+        @notebook.add_page(@server_page, 'Server Info', false)
+        @notebook.add_page(@browser_page, 'Key Browser', true)
       end
 
       def setup_status_bar
--- lib/rediscover/panel/browser.rb
+++ lib/rediscover/panel/browser.rb
@@ -5,14 +5,16 @@ module Rediscover
     class Browser < Wx::Panel
       include Wx
 
-      def initialize(window)
-        super(window, -1)
+      def initialize(parent)
+        @parent = parent
+        super(@parent, -1)
 
         @redis = get_app.redis
         @logger = get_app.logger
         @key_pattern = '*'
 
         setup_sizer
+        setup_tool_bar
         setup_key_browser
         setup_key_shortcuts
       end
@@ -20,6 +22,19 @@ module Rediscover
       def setup_sizer
         @sizer = BoxSizer.new(VERTICAL)
         set_sizer(@sizer)
+      end
+	  
+      def setup_tool_bar
+        @tool_bar_sizer = BoxSizer.new(HORIZONTAL)
+        @sizer.add_item(@tool_bar_sizer)
+        @filter_textbox = TextCtrl.new(self,
+                                       :style => TE_PROCESS_ENTER,
+                                       :size => Size.new(200, -1))
+        evt_text_enter @filter_textbox, :filter # filter when enter is pressed in the textbox
+        @filter_button = Button.new(self, :label => 'Filter')
+        evt_button @filter_button, :filter # filter when button is pressed
+        @tool_bar_sizer.add_item(@filter_textbox)
+        @tool_bar_sizer.add_item(@filter_button)
       end
 
       def setup_key_shortcuts
